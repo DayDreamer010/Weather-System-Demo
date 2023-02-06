9fdd31cc23e587c32dd329a53d3229965a70e749
Vismay thaker <vismaythaker008@gmail.com>
2023-02-07 01:32:06 +0530

    Initial commit

---
+++ Assets/RPGPP_LT/Script/LightingManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+  
+    [SerializeField] private Light DirectionalLight;
+     [SerializeField, Range(0, 218)] private float TimeOfDay;
+    [SerializeField] private Light[] lightPoint;
+    [SerializeField] private SpriteRenderer Star;
+    [SerializeField] private Animator S1;
+
+
+
+
+
+    private void Update()
+    {
+      
+
+        if (Application.isPlaying)
+        {
+            TimeOfDay += Time.deltaTime;
+            TimeOfDay %= 218;
+         
+            UpdateLighting(TimeOfDay / 218f);
+            if (TimeOfDay>=50)
+            {
+                for (int i = 0; i < lightPoint.Length; i++)
+                {
+                    if (TimeOfDay >= 180)
+                    {
+                        lightPoint[i].enabled = true;
+                        S1.SetBool("FadeIn", true);
+                    }
+                    else
+                    {
+                        lightPoint[i].enabled = false;
+                        S1.enabled = true;
+                      
+                       
+                    }
+                  
+                }
+                
+                
+            }
+            
+           
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 218f);
+        }
+    }
+
+    public IEnumerator FadeOut(float fadeSpeed)
+    {
+        SpriteRenderer rend = Star;
+        Color matColor = Star.color;
+        float alphaValue = rend.color.a;
+
+
+        while (rend.material.color.a > 0f)
+        {
+            alphaValue -= Time.deltaTime / fadeSpeed;
+            rend.material.color = new Color(matColor.r, matColor.g, matColor.b, alphaValue);
+            yield return new WaitForSeconds(1f);
+        }
+        rend.material.color = new Color(matColor.r, matColor.g, matColor.b, 0f);
+        
+    }
+
+
+    private void UpdateLighting(float timePercent)
+    {    if (DirectionalLight != null)
+        {
+        
+
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+
+    }
+
+
+
+    
+    private void OnValidate()
+    {
+        if (DirectionalLight != null)
+            return;
+
+        
+        if (RenderSettings.sun != null)
+        {
+            DirectionalLight = RenderSettings.sun;
+        }
+       
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    DirectionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+
+}
---
+++ Assets/RPGPP_LT/Script/WeatherSystem.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherSystem : MonoBehaviour
+{
+   // [SerializeField] private GameObject rain;
+    [SerializeField] private ParticleSystem rain1;
+    public AudioSource rainAudio;
+
+
+
+    public void Start()
+    {
+        rainAudio.Stop();
+        int isRain = Random.Range(0, 2);
+        Debug.Log("number" + isRain);
+        if (isRain == 0)
+        {
+            rain1.enableEmission = false;
+        }
+        else
+        {
+            StartCoroutine(RainStart());
+
+        }
+        StartCoroutine(WeatherCheck());
+       
+    }
+
+    IEnumerator WeatherCheck()
+    {
+       
+       
+        yield return new WaitForSeconds(30f);
+        if (rain1.enableEmission == false)
+        {
+            StartCoroutine(RainStart());
+        }
+        else
+        {
+            StartCoroutine(RainStop());
+        }
+        yield return new WaitForSeconds(30f);
+        StartCoroutine(WeatherCheck());
+    }
+    IEnumerator RainStart()
+    {
+        rain1.enableEmission = true;
+        var p1 = rain1.emission;
+        rainAudio.Play();
+        p1.rateOverTime = 200;
+        rainAudio.volume = 0.5f;
+        yield return new WaitForSeconds(10f);
+        p1.rateOverTime = 2000;
+        rainAudio.volume = 1f;
+        rainAudio.enabled = true;
+    }
+
+
+    IEnumerator RainStop()
+    {
+        rainAudio.Play();
+        rain1.enableEmission = true;
+        var p1 = rain1.emission;
+        p1.rateOverTime = 900;
+        rainAudio.volume = 0.5f;
+        yield return new WaitForSeconds(10f);
+        p1.rateOverTime = 200;
+        rainAudio.volume = 0.2f;
+        yield return new WaitForSeconds(2f);
+        rain1.enableEmission = false;
+        rainAudio.Stop();
+
+    }
+    private void Update()
+    {
+       
+
+
+
+
+
+
+    }
+}
+
+    
+
+
+
+   
